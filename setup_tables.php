733e6298079aeae53d681a928cd64e24a580cf92
nadinad <nadinadevabalan@gmail.com>
2025-02-03 16:05:19 +0000

    debug

--- setup_tables.php
+++ setup_tables.php
@@ -105,15 +105,15 @@ $pass = $config['DB_PASS'];
 try {
     // Connect to the database
     $pdo = new PDO(
-        dsn: "mysql:host=$host;dbname=$db;charset=utf8mb4",
-        username: $user,
-        password: $pass
+        "mysql:host=$host;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass
     );
     // Set the PDO error mode to exception
     // If an error occurs, it will throw an exception
     $pdo->setAttribute(
-        attribute: PDO::ATTR_ERRMODE,
-        value: PDO::ERRMODE_EXCEPTION
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
     );
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
