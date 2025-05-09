4952b4cbd3fe375410af50b7fb71e28716c6ffae
Nadina Devabalan <89397179+NadinaD06@users.noreply.github.com>
2025-05-09 20:57:12 +0100

    changes.3

--- httpdocs/config/config.php
+++ httpdocs/config/config.php
@@ -14,4 +14,4 @@ define('UPLOAD_DIR', PUBLIC_PATH . '/uploads');
 define('DB_HOST', 'localhost:3306');  // Added port number
 define('DB_NAME', 's4413713_ctxxxx');  // Your Plesk database name
 define('DB_USER', 's4413713_ctxxxx');  // Your Plesk database username
-define('DB_PASS', '');  // Your Plesk database password 
+define('DB_PASS', '83N*psb81');  // Your Plesk database password 
