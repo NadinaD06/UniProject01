6a517b720e932bfdd37ca962b8681e6b6ce5f85f
nadinad <nadinadevabalan@gmail.com>
2025-05-09 20:26:18 +0100

    test.7

--- httpdocs/test_paths.php
+++ httpdocs/test_paths.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Load configuration
-$configFile = __DIR__ . '/config/config.php';
+$configFile = dirname(dirname(__FILE__)) . '/config/config.php';
 if (!file_exists($configFile)) {
     die("Configuration file not found at: " . $configFile);
 }
